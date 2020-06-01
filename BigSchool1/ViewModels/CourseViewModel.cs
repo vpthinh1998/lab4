5c54c5a7280fa766ef1aa133626defa94a89b63d
Administrator <Administrator@PC250>
2020-06-01 11:37:15 +0700

    Add project files.

---
+++ BigSchool1/ViewModels/CourseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool1.Models
+{
+    public class CourseViewModel
+    {
+        
+        public string Place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+    }
+
+    
+}
